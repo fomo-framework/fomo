05f4b0883a62efe7fa8a95a48e69ed749236abf3
Amir <faramarzii.amir@gmail.com>
2021-05-08 18:03:25 +0430

    add database config

---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'mysql' => [
+        'driver' => 'mysql',
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'database' => 'tower',
+        'username' => 'root',
+        'password' => 'root',
+        'unix_socket' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+        'strict' => true,
+        'engine' => null,
+    ]
+];
