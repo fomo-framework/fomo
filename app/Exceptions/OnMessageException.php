83c9f01dbb85ce6779c4b7b5262b20173f0e29e0
Amir <faramarzii.amir@gmail.com>
2021-06-07 12:31:23 +0430

    update OnMessageException

--- app/Exceptions/OnMessageException.php
+++ app/Exceptions/OnMessageException.php
@@ -1,11 +1,17 @@
 <?php
 namespace App\Exceptions;
 
+use Throwable;
 use Tower\Exception\Contract;
 use Tower\Response;
 
 class OnMessageException extends \Exception implements Contract
 {
+    public function __construct(Throwable $error)
+    {
+        parent::__construct($error->getMessage() , $error->getCode(), $error->getPrevious());
+    }
+
     public function handle(): Response
     {
         return json([
