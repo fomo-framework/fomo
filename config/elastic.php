b15dd43fe7ec77c8beb5cce75a0f4b46ae15a19c
Amir <faramarzii.amir@gmail.com>
2021-06-15 17:48:33 +0430

    bug fix

--- config/elastic.php
+++ config/elastic.php
@@ -4,7 +4,7 @@ return [
     [
         'host' => '127.0.0.1',
         'port' => '9200',
-//    'user' => null,
-//    'pass' => null
+        'user' => null,
+        'pass' => null
     ]
 ];
--- config/redis.php
+++ config/redis.php
@@ -2,6 +2,7 @@
 
 return [
     'host' => '127.0.0.1',
+    'username' => null,
     'password' => null,
     'port' => 6379,
     'database' => 0,
