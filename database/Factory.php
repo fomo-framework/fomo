124c47d23dae49ec4270603d1c083a4fc9dce564
Amir <faramarzii.amir@gmail.com>
2021-08-21 19:08:38 +0430

    add new class database/Factory and set to composer

---
+++ database/Factory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database;
+
+use Tower\Forger;
+use Tower\Inserter;
+
+class Factory
+{
+    public function run()
+    {
+//        $forger = Forger::create();
+//
+//        (new Inserter('users' , [
+//            'first_name' => $forger->firstName() ,
+//            'last_name' => $forger->lastName() ,
+//            'email' => $forger->email() ,
+//        ]))->create(1);
+//
+//        (new Inserter('users' , [
+//            'first_name' => $forger->firstName() ,
+//            'last_name' => $forger->lastName() ,
+//            'email' => $forger->email() ,
+//        ]))->connection('elastic')->create(1);
+    }
+}
