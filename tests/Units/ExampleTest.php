47dba5e1750e93029468cd415376bdbcd75ea62d
Amir <faramarzii.amir@gmail.com>
2021-08-22 17:00:21 +0430

    add folder tests in base project

---
+++ tests/Units/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Units;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
