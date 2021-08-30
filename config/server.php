b5958a7f25baaa42cab292ca64675c494aa22e57
Amir <faramarzii.amir@gmail.com>
2021-08-30 14:12:29 +0430

    refactor config/server.php

--- config/server.php
+++ config/server.php
@@ -1,17 +1,12 @@
 <?php
 
 return [
-    'listen'               => 'http://127.0.0.1:9000',
-    'timezone'             => env('APP_TIMEZONE' , 'UTC'),
-    'faker_locale'         => 'en_US',
-    'transport'            => 'tcp',
-    'context'              => [],
-    'name'                 => env('APP_NAME' , 'Tower'),
-    'count'                => env('APP_WORKER_COUNT' , cpuCoreCount() * 2) ,
-    'user'                 => '',
-    'group'                => '',
-    'pid_file'             => storagePath() . 'tower.pid',
-    'max_request'          => 1000000,
-    'stdout_file'          => storagePath() . 'logs/tower.log',
-    'max_package_size'     => 10*1024*1024
+    'listen' => 'http://127.0.0.1:9000',
+    'transport' => 'tcp',
+    'context' => [],
+    'count' => env('APP_WORKER_COUNT', cpuCoreCount() * 2),
+    'user' => '',
+    'group' => '',
+    'max_request' => 1000000,
+    'max_package_size' => 10 * 1024 * 1024
 ];
