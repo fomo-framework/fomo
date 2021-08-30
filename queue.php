ec1ce8f5ac745aa7473a2c006cb39385d474a528
Amir <faramarzii.amir@gmail.com>
2021-08-30 14:14:01 +0430

    refactory queue.php

--- queue.php
+++ queue.php
@@ -2,22 +2,53 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Dotenv\Dotenv;
+use Tower\Console\Color;
 use Tower\JobWorker;
+use Tower\Unemployed;
+use Workerman\Connection\TcpConnection;
 use Workerman\Worker;
 
-$dotenv = Dotenv\Dotenv::createImmutable(basePath());
-$dotenv->load();
+$watch = 0;
+$daemonize = 0;
 
-$config = include configPath() . "server.php";
+foreach ($argv as $value){
+    if ($value == '--watch')
+        $watch = 1;
+    if ($value == '-d')
+        $daemonize = 1;
+}
+
+if ($watch == 1 && $daemonize == 1){
+    echo Color::error('daemon mode cannot be used in development mode');
+    return;
+}
+
+$serverConfig = include configPath() . "server.php";
+$appConfig = include configPath() . "app.php";
+
+Dotenv::createImmutable(basePath())->load();
+
+Worker::$pidFile = storagePath() . 'queue.pid';
+Worker::$stdoutFile = storagePath() . 'logs/queue.log';
+TcpConnection::$defaultMaxPackageSize = $serverConfig['max_package_size'] ?? 10*1024*1024;
 
 $worker = new Worker();
+$worker->name = 'queue';
 
-$worker->name = $config['name'];
-
-date_default_timezone_set($config['timezone']);
+date_default_timezone_set($appConfig['timezone']);
 
 $jobWorker = new JobWorker();
 
-$worker->onWorkerStart = [$jobWorker, 'workerRun'];
+$worker->onWorkerStart = [$jobWorker, 'onWorkerStart'];
+
+if ($watch == 1 && $argv[1] == 'start'){
+    $worker = new Worker();
+    $unemployed = new Unemployed();
+
+    $worker->name = 'unemployed man';
+
+    $worker->onWorkerStart = [$unemployed , 'check'];
+}
 
 Worker::runAll();
