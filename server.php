dbbeac47a356d82baf4443f6fb40b66729512668
Amir <faramarzii.amir@gmail.com>
2021-08-30 14:16:12 +0430

    refactor server.php

--- server.php
+++ server.php
@@ -1,23 +1,41 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
 use Tower\Application;
+use Tower\Console\Color;
+use Tower\Unemployed;
 use Workerman\Connection\TcpConnection;
 use Workerman\Worker;
 
-require_once __DIR__ . '/vendor/autoload.php';
+$watch = 0;
+$daemonize = 0;
 
-$dotenv = Dotenv\Dotenv::createImmutable(basePath());
-$dotenv->load();
+foreach ($argv as $value){
+    if ($value == '--watch')
+        $watch = 1;
+    if ($value == '-d')
+        $daemonize = 1;
+}
 
+if ($watch == 1 && $daemonize == 1){
+    echo Color::error('daemon mode cannot be used in development mode');
+    return;
+}
 
-$config = include configPath() . "server.php";
+$serverConfig = include configPath() . "server.php";
+$appConfig = include configPath() . "app.php";
 
-Worker::$pidFile = $config['pid_file'];
-Worker::$stdoutFile = $config['stdout_file'];
-TcpConnection::$defaultMaxPackageSize = $config['max_package_size'] ?? 10*1024*1024;
+Dotenv::createImmutable(basePath())->load();
 
-$http_worker = new Worker($config['listen'], $config['context']);
-$property_map = [
+Worker::$pidFile = storagePath() . 'tower.pid';
+Worker::$stdoutFile = storagePath() . 'logs/tower.log';
+TcpConnection::$defaultMaxPackageSize = $serverConfig['max_package_size'] ?? 10*1024*1024;
+
+$worker = new Worker($serverConfig['listen'], $serverConfig['context']);
+
+$properties = [
     'name',
     'count',
     'user',
@@ -25,19 +43,25 @@ $property_map = [
     'reusePort',
     'transport',
 ];
-foreach ($property_map as $property) {
-    if (isset($config[$property])) {
-        $http_worker->$property = $config[$property];
-    }
-}
+foreach ($properties as $property)
+    if (isset($serverConfig[$property]))
+        $worker->$property = $serverConfig[$property];
 
-date_default_timezone_set($config['timezone']);
-
+date_default_timezone_set($appConfig['timezone']);
 
 $boot = new Application();
 
-$http_worker->onWorkerStart = [$boot , 'onWorkerStart'];
+$worker->onWorkerStart = [$boot , 'onWorkerStart'];
 
-$http_worker->onMessage = [$boot , 'onMessage'];
+$worker->onMessage = [$boot , 'onMessage'];
+
+if ($watch == 1 && $argv[1] == 'start'){
+    $worker = new Worker();
+    $unemployed = new Unemployed();
+
+    $worker->name = 'unemployed man';
+
+    $worker->onWorkerStart = [$unemployed , 'check'];
+}
 
 Worker::runAll();
