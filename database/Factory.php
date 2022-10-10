9a64376f10ea128bddc33fb4481a41b2caf98cb7
Amir <faramarzii.amir@gmail.com>
2022-10-10 11:59:22 +0330

    new Database\Factory

---
+++ database/Factory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database;
+
+use Fomo\Fake\Faker;
+use Fomo\Inserter\Inserter;
+
+class Factory
+{
+    public function run()
+    {
+//        $faker = Faker::create();
+//
+//        (new Inserter('users' , [
+//            'first_name' => $faker->firstName() ,
+//            'last_name' => $faker->lastName() ,
+//            'email' => $faker->email() ,
+//        ]))->create(1);
+//
+//        (new Inserter('users' , [
+//            'first_name' => $faker->firstName() ,
+//            'last_name' => $faker->lastName() ,
+//            'email' => $faker->email() ,
+//        ]))->connection('elastic')->create(1);
+    }
+}
