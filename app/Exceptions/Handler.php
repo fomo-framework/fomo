a55e2c0d681b60c2a52506f50962362172f25def
Amir <faramarzii.amir@gmail.com>
2024-07-12 17:20:14 +0330

    fix exceptions/handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,26 +3,25 @@
 namespace App\Exceptions;
 
 use Fomo\Request\Request;
-use Fomo\Response\Response;
 use Throwable;
 
 class Handler
 {
-    public function notFoundHttpException(Request $request): Response
+    public function notFoundHttpException(Request $request): string
     {
         return response()->json([
             'message' => 'not found'
         ] , 404);
     }
 
-    public function notAllowedHttpException(Request $request): Response
+    public function notAllowedHttpException(Request $request): string
     {
         return response()->json([
             'message' => "this is route supported {$request->method()} method"
         ] , 405);
     }
 
-    public function InternalErrorException(Request $request, Throwable $error): Response
+    public function InternalErrorException(Request $request, Throwable $error): string
     {     
         return response()->json([
             'message' => 'internal error'
